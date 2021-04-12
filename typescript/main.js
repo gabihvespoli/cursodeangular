99ab63b3fbd9d4a98e9ffcbd98d16a924a02a5f5
Gabriela Vespoli <gabiat3@hotmail.com>
2021-04-11 23:01:32 -0300

    folders update

---
+++ typescript/main.js
@@ -0,0 +1,16 @@
+var minhaVar = 'var feliz';
+function minhaFunc(x, y) {
+    return x + y;
+}
+var numeros = [1, 2, 3];
+numeros.map(function (valor) { return valor * 2; });
+var Matematica = /** @class */ (function () {
+    function Matematica() {
+    }
+    Matematica.prototype.soma = function (x, y) {
+        return x + y;
+    };
+    return Matematica;
+}());
+var n1 = 'ssdf';
+n1 = 1;
---
+++ typescript/main.ts
@@ -0,0 +1,16 @@
+let minhaVar = 'var feliz'
+
+function minhaFunc(x, y) {
+  return x + y
+}
+let numeros = [1, 2, 3]
+
+numeros.map(valor => valor * 2)
+
+class Matematica {
+  soma (x, y){
+    return x + y
+  }
+}
+
+var n1 = 'ssdf'
